16b9753b19efd8e7d9212dc457751e843f76a2d3
8121117543 <41770130+8121117543@users.noreply.github.com>
2018-08-04 21:23:36 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,8 +1,8 @@
 ch =raw_input();
 if((ch>=a and ch<=z) or (ch>=A and ch<=Z)):
 	if(ch==A or ch==a or ch==E or ch ==e or ch==I or ch==i or ch==O or ch==o or ch==U or ch==u):
-		print(ch, "is a Vowel")
+		print(ch, "Vowel")
 	else:
-		print(ch, "is a Consonant") 
+		print(ch, "Consonant") 
 else:
-     print(ch,"is invalid")
+     print(ch,"invalid")
