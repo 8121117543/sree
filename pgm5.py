d1c630ac984a13a728ff11390d39506fde0c6e1a
8121117543 <41770130+8121117543@users.noreply.github.com>
2018-08-23 12:43:08 +0530

    Update pgm5.py

--- pgm5.py
+++ pgm5.py
@@ -1,7 +1,8 @@
-n1,n2,n3=input("enter three numbers").splti()
-if(n1>=n2) or (n1>=n3):
-                      print(n1)
-elif(n2>=n3) or (n2>=n1):
-                        print(n2)
-  else:
-      print(n3)
+n1,n2,n3=input().split()
+if (n1>n2) and (n1>n3):
+	num=n1
+elif (n2>n3) and (n2>n1):
+            num=n2	
+else:
+            num=n3								
+print(num)
