4207d8e2e8512866a78ea34e558e772ccc88797d
8121117543 <41770130+8121117543@users.noreply.github.com>
2018-08-23 12:07:32 +0530

    Update pgm5.py

--- pgm5.py
+++ pgm5.py
@@ -1,1 +1,7 @@
 n1,n2,n3=input("enter three numbers").splti()
+if(n1>=n2) or (n1>=n3):
+                      print(n1)
+elif(n2>=n3) or (n2>=n1):
+                        print(n2)
+  else:
+      print(n3)
